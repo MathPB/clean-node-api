cf85bcae4409293a6f22534f5177eec0c47e1457
MathPB <matheus.pereira4025@hotmail.com>
2021-06-27 19:09:20 -0300

    feat: ensure SingUp Controller if no password provided

--- src/presentation/controllers/singup.ts
+++ src/presentation/controllers/singup.ts
@@ -3,7 +3,7 @@ import { badRequest } from "../helpers/http-helper";
 import { MissingParamError } from "../erros/missing-param-error";
 export class SingUpController {
   handle(httpRequest: HttpRequest): HttpResponse {
-    const requiredFields = ["name", "email"];
+    const requiredFields = ["name", "email", "password"];
     for (const field of requiredFields) {
       if (!httpRequest.body[field]) {
         return badRequest(new MissingParamError(field));
