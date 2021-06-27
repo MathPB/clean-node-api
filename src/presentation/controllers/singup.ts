4e623adbd5be1230ffa1a0b6b85f8456ea43ada4
MathPB <matheus.pereira4025@hotmail.com>
2021-06-27 19:19:01 -0300

    refactor: make SingUpController conforms to a protocol

--- src/presentation/controllers/singup.ts
+++ src/presentation/controllers/singup.ts
@@ -1,7 +1,8 @@
 import { HttpRequest, HttpResponse } from "../protocols/http";
 import { badRequest } from "../helpers/http-helper";
 import { MissingParamError } from "../erros/missing-param-error";
-export class SingUpController {
+import { Controller } from "../protocols/controller";
+export class SingUpController implements Controller {
   handle(httpRequest: HttpRequest): HttpResponse {
     const requiredFields = [
       "name",
