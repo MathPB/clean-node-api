6a1e5acc9ef89fe39ae8b227b0d685c859c388db
MathPB <matheus.pereira4025@hotmail.com>
2021-06-27 15:44:24 -0300

    refactor: add customized error for missing param

--- src/presentation/controllers/singup.spec.ts
+++ src/presentation/controllers/singup.spec.ts
@@ -1,4 +1,5 @@
 import { SingUpController } from "./singup";
+import { MissingParamError } from "../erros/missing-param-error";
 
 describe("SingUpController", () => {
   test("Should return 400 if no name is provided", () => {
@@ -13,7 +14,7 @@ describe("SingUpController", () => {
 
     const httpResponse = sut.handle(httpRequest);
     expect(httpResponse.statusCode).toBe(400);
-    expect(httpResponse.body).toEqual(new Error("Missing param: name"));
+    expect(httpResponse.body).toEqual(new MissingParamError("name"));
   });
 
   test("Should return 400 if no name is provided", () => {
@@ -28,6 +29,6 @@ describe("SingUpController", () => {
 
     const httpResponse = sut.handle(httpRequest);
     expect(httpResponse.statusCode).toBe(400);
-    expect(httpResponse.body).toEqual(new Error("Missing param: email"));
+    expect(httpResponse.body).toEqual(new MissingParamError("email"));
   });
 });
